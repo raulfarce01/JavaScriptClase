06a5cc3c62cccdf7c4e9803b71c8e993cbdc9fcf
Neiroh <raulito72001@gmail.com>
2021-09-28 13:04:02 +0200

    weee

---
+++ js/cambiaPic.js
@@ -0,0 +1,9 @@
+function cambiaPic(){
+
+                        let miImagen=document.getElementById('imagen');
+                        if(miImagen.src.match("./img/imagen1.jpg")){
+                                miImagen.src="./img/imagen2.jpg";
+                        }else{
+                                miImagen.src="./img/imagen1.jpg";
+                        }
+}
