c31c16019cffcf0e8874233ee00afc7485d8c460
Neiroh <raulito72001@gmail.com>
2021-10-04 11:44:25 +0200

    Primer commit de Raúl Fernández Arce

--- js/cambiaPic.js
+++ js/cambiaPic.js
@@ -1,9 +1,9 @@
-function cambiaPic(){
+function cambiaPic() {
 
-                        let miImagen=document.getElementById('imagen');
-                        if(miImagen.src.match("./img/imagen1.jpg")){
-                                miImagen.src="./img/imagen2.jpg";
-                        }else{
-                                miImagen.src="./img/imagen1.jpg";
-                        }
+    let miImagen = document.getElementById('imagen');
+    if (miImagen.src.match("./img/imagen1.jpg")) {
+        miImagen.src = "./img/imagen2.jpg";
+    } else {
+        miImagen.src = "./img/imagen1.jpg";
+    }
 }
